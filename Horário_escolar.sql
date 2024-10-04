de6e5ae62a342481e54a05637315c289cfd8143d
Gabizinha1002 <gabriella.marques.silva@escola.pr.gov.br>
2024-10-03 21:57:57 -0300

    Update Horário_escolar.sql

--- Horário_escolar.sql
+++ Horário_escolar.sql
@@ -1,1 +1,19 @@
+CREATE TABLE horário (
+    id INTEGER PRIMARY KEY,
+    Horários TEXT NOT NULL,
+    Segunda TEXT NOT NULL,
+    Terça TEXT NOT NULL,
+    Quarta TEXT NOT NULL,
+    Quinta TEXT NOT NULL,
+    Sexta TEXT NOT NULL
+);
 
+INSERT INTO horário (Horários, Segunda, Terça, Quarta, Quinta, Sexta) VALUES
+('07:30', 'Biologia', 'Matemática', 'Matemática', 'Química', 'Programação Mobile')
+('08:20', 'Sociologia', 'Português', 'Português', 'Inglês', 'Banco de Dados')
+('09:10', 'Projeto de Vida', 'Programação Front-end', 'Programação Front-end', 'Química', 'Matemática')
+('10:20', 'Análise e Projeto de Sistemas', 'História', 'Programação Front-end', 'Geografia', 'Biologia')
+('11:10', 'Análise e Projeto de Sistemas', 'Ciência da Computação', 'Ed Financeira', 'Programação Front-end', 'Programação Mobile')
+('12:00', 'Sociologia', 'Português', 'Banco de Dados', 'Geografia', 'Ciência da Computação')
+
+ 
